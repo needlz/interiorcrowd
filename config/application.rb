246d48ca248d847910399ba1dbfea4e42c4fedf7
RomanSokhan <romans@softwareplanet.uk.com>
2014-11-13 10:28:42 +0200

    configure access to Amazon S3

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,15 @@ module InteriorC
       :enable_starttls_auto => true  
    }
 
+
+   config.paperclip_defaults = {
+       url: ':s3_domain_url',
+       storage: :s3,
+       s3_credentials: {
+           bucket: ENV['S3_BUCKET_NAME'],
+           access_key_id: ENV['AWS_ACCESS_KEY'],
+           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+       }
+   }
   end
 end
