35ba57a5cb4efc2283e16d5fbc5f8673e1b9de9f
RomanSokhan <romans@softwareplanet.uk.com>
2014-11-17 10:50:16 +0200

    optimize queries

--- app/models/design_space.rb
+++ app/models/design_space.rb
@@ -3,13 +3,10 @@ class DesignSpace < ActiveRecord::Base
 
   has_many :contests
   belongs_to :parent, class_name: 'DesignSpace', foreign_key: :parent_id
+  has_many :children, class_name: 'DesignSpace', foreign_key: :parent_id
 
   scope :available, where(status: ACTIVE_STATUS).order(pos: :asc)
   scope :top_level, where(parent_id: 0)
   scope :by_ids, ->(ids) { where("id IN (?)", ids).order(:pos) }
-
-  def children
-    DesignSpace.where(parent_id: id)
-  end
 
 end
