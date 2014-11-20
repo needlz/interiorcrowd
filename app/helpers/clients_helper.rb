9535e12da6349f2bfb0c0faf80860e19d5e8de8c
RomanSokhan <romans@softwareplanet.uk.com>
2014-11-20 15:33:37 +0200

    move partials to appropriate folders

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -12,17 +12,17 @@ module ClientsHelper
 
   def brief_fields
     [
-      { option: 'category', option_partial: 'contests/categories_preview' },
-      { option: 'area', option_partial: 'contests/areas_preview' },
-      { option: 'appeals', option_partial: 'contests/appeals_preview' },
-      { option: 'desirable_colors', option_partial: 'contests/desirable_colors_preview' },
-      { option: 'undesirable_colors', option_partial: 'contests/undesirable_colors_preview' },
-      { option: 'example_pictures', option_partial: 'contests/examples_preview' },
-      { option: 'example_links', option_partial: 'contests/links_preview' },
-      { option: 'space_pictures', option_partial: 'contests/space_pictures_preview' },
-      { option: 'space_dimensions', option_partial: 'contests/dimensions_preview' },
-      { option: 'budget', option_partial: 'contests/budget_preview' },
-      { option: 'feedback', option_partial: 'contests/comment_preview' },
+      { option: 'category', option_partial: 'contests/previews/categories_preview' },
+      { option: 'area', option_partial: 'contests/previews/areas_preview' },
+      { option: 'appeals', option_partial: 'contests/previews/appeals_preview' },
+      { option: 'desirable_colors', option_partial: 'contests/previews/desirable_colors_preview' },
+      { option: 'undesirable_colors', option_partial: 'contests/previews/undesirable_colors_preview' },
+      { option: 'example_pictures', option_partial: 'contests/previews/examples_preview' },
+      { option: 'example_links', option_partial: 'contests/previews/links_preview' },
+      { option: 'space_pictures', option_partial: 'contests/previews/space_pictures_preview' },
+      { option: 'space_dimensions', option_partial: 'contests/previews/dimensions_preview' },
+      { option: 'budget', option_partial: 'contests/previews/budget_preview' },
+      { option: 'feedback', option_partial: 'contests/previews/comment_preview' },
     ]
   end
 
