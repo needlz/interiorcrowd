3698457c407f579eb33fb74674454ee28c955b94
YaroslavO <yaroslavo@softwareplanet.uk.com>
2015-01-29 17:05:37 +0200

    add page for faq (customer and designer) add route for faq page add home helper for rendering questions/answers

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,23 @@
+module HomeHelper
+
+  def faq_customer_about_ic
+    t('faq.customer.questions_about_ic')
+  end
+
+  def faq_customer_payment
+    t('faq.customer.questions_payment')
+  end
+
+  def faq_customer_touch_ic
+    t('faq.customer.questions_touch_ic')
+  end
+
+  def faq_designer_touch_ic
+    t('faq.designer.questions_touch_ic')
+  end
+
+  def faq_designer_payment
+    t('faq.designer.questions_payment')
+  end
+
+end
