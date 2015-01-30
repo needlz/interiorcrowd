73722255dc257d0a4cf24095c2d78acfcfd41028
YaroslavO <yaroslavo@softwareplanet.uk.com>
2015-01-30 14:18:13 +0200

    change layout for responses page in designer center

--- app/view_objects/contest_short_details.rb
+++ app/view_objects/contest_short_details.rb
@@ -1,13 +1,14 @@
 class ContestShortDetails
   include ActionView::Helpers::DateHelper
 
-  attr_reader :id, :name, :design_category, :design_space, :days_left, :price
+  attr_reader :id, :name, :design_category, :design_space, :days_left, :price, :days_count
 
   def initialize(contest)
     @id = contest.id
     @name = contest.project_name
     @design_category = contest.design_category.name
     @design_space = contest.design_space.name
+    @days_count = contest.days_left
     @days_left =
       if contest.submission?
         I18n.t('designer_center.contests_preview.days_left',
