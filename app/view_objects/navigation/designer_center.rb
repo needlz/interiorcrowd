05e7e8167b0f9c15fac16c70d8f12c255c77fcd2
RomanSokhan <romans@softwareplanet.uk.com>
2015-02-05 15:25:50 +0200

    Change name and order of navigation tabs in designer center

--- app/view_objects/navigation/designer_center.rb
+++ app/view_objects/navigation/designer_center.rb
@@ -3,12 +3,12 @@ module Navigation
   class DesignerCenter < Base
     def tabs
       {
-        portfolio: { name: I18n.t('designer_center.navigation.create_portfolio'),
-          href: edit_portfolio_path },
+        requests: { name: I18n.t('designer_center.navigation.respond'),
+                      href: designer_center_response_index_path},
         contests: { name: I18n.t('designer_center.navigation.preview_contests'),
           href: designer_center_contest_index_path},
-        requests: { name: I18n.t('designer_center.navigation.respond'),
-          href: designer_center_response_index_path},
+        portfolio: { name: I18n.t('designer_center.navigation.create_portfolio'),
+                     href: edit_portfolio_path },
         updates: { name: I18n.t('designer_center.navigation.updates'),
                     href: updates_designer_center_index_path}
       }
