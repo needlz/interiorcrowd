bd68e158839e257a7de5d05e2d16a96cfd58914a
pavlov <pavlov@pavlov-lviv-pc.(none)>
2015-02-09 16:35:55 +0200

    refactor code

--- app/controllers/designer_center_controller.rb
+++ app/controllers/designer_center_controller.rb
@@ -2,12 +2,12 @@ class DesignerCenterController < ApplicationController
   before_filter :set_designer
 
   def designer_center
-      if @designer.portfolio.complete?
-        return redirect_to designer_center_response_index_path if @designer.has_active_requests?
-        redirect_to designer_center_contest_index_path
-      else
-        redirect_to edit_portfolio_path
-      end
+    if @designer.portfolio.complete?
+      return redirect_to designer_center_response_index_path if @designer.has_active_requests?
+      redirect_to designer_center_contest_index_path
+    else
+      redirect_to edit_portfolio_path
+    end
   end
 
   def updates
