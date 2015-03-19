2c862670b16d77991611db49f300dec64983b1c1
RomanSokhan <romans@softwareplanet.uk.com>
2015-03-19 18:37:54 +0200

    Fix rendering of contests index in designer center

--- spec/models/available_contests_query_spec.rb
+++ spec/models/available_contests_query_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe AvailableContestsQuery do
 
   let(:closed_contest) { Fabricate(:contest, client: client, status: 'closed') }
   let(:submission_contest) { Fabricate(:contest, client: client, status: 'submission') }
+  let(:invited_contest) { Fabricate(:contest, client: client, status: 'submission') }
   let(:winner_selection_contest) { Fabricate(:contest, client: client, status: 'winner_selection') }
   let(:fulfillment_contest) { Fabricate(:contest, client: client, status: 'fulfillment') }
   let(:finished_contest) { Fabricate(:contest, client: client, status: 'finished') }
@@ -16,18 +17,18 @@ RSpec.describe AvailableContestsQuery do
   let(:available_contests) { AvailableContestsQuery.new(designer) }
 
   it 'returns list of active contests' do
-    expect(available_contests.all).to match_array [submission_contest]
+    expect(available_contests.all).to match_array [submission_contest, invited_contest]
   end
 
   context 'invited to contests' do
     before do
-      contests.each do |contest|
+      [invited_contest, winner_selection_contest].each do |contest|
         Fabricate(:designer_invite_notification, designer: designer, contest: contest)
       end
     end
 
     it 'returns list of current contests a designer was invited to' do
-      expect(available_contests.invited).to match_array [submission_contest]
+      expect(available_contests.invited).to match_array [invited_contest]
     end
   end
 
