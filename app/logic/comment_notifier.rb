7787c5ace12a0da02d4563cf4666b92d84657144
pavlov <pavlov@pavlov-lviv-pc.(none)>
2015-04-17 14:45:41 +0300

    Add notification about comment on board

---
+++ app/logic/comment_notifier.rb
@@ -0,0 +1,41 @@
+class CommentNotifier
+
+  def initialize(contest_request, user)
+    @contest_request = contest_request
+    @user_role = user.class.name.downcase
+
+  end
+
+  def perform
+    delayed_job = delayed_job_for_email
+    delayed_job ? delay_sending_time(delayed_job) : send_email
+  end
+
+  private
+
+  attr_reader :user_role, :contest_request
+
+  def send_email
+    Jobs::Mailer.schedule("comment_on_board",
+                          [{ username: user.name, email: user.email, role: user_role, search_by: "#{user_role}s_comment_on_board" }, contest_request.id],
+                          { run_at: digest_minutes_interval, contest_request_id: contest_request.id })
+  end
+
+  def delayed_job_for_email
+    Delayed::Job.where('handler LIKE ? and contest_request_id = ?', "%#{user_role}s_comment_on_board%", contest_request.id).first
+  end
+
+  def delay_sending_time(delayed_job)
+    delayed_job.update_attribute(:run_at, digest_minutes_interval)
+  end
+
+  def digest_minutes_interval
+    Settings.comment_board_digest_minutes_interval.to_i.minutes.from_now.utc
+  end
+
+  def user
+    return contest_request.designer if user_role == 'client'
+    contest_request.contest.client
+  end
+
+end
