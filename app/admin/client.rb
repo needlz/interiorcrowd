29d0935e1b7de13f5cad5c9ef8278b48bdf2e2bb
pavlov <pavlov@pavlov-lviv-pc.(none)>
2015-04-24 10:17:15 +0300

    Add checkbox  instead of input to activeadmin for roles_mask field

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -14,5 +14,34 @@ ActiveAdmin.register Client do
   #   permitted
   # end
 
+  form do |f|
+    f.inputs do
+      f.input :id
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+      f.input :password
+      f.input :name_on_card
+      f.input :card_type
+      f.input :address
+      f.input :state
+      f.input :zip
+      f.input :card_number
+      f.input :card_ex_month
+      f.input :card_ex_year
+      f.input :card_cvc
+      f.input :status
+      f.input :city
+      f.input :phone_number
+      f.input :billing_address
+      f.input :billing_state
+      f.input :billing_city
+      f.input :plain_password
+      f.input :roles_mask, :as => :check_boxes, label: 'Role', :collection => [['Test', 1]] #TODO value is not saved, fix it
+      f.input :created_at
+      f.input :updated_at
+    end
+    f.actions
+  end
 
 end
