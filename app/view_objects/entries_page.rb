097cc5ffa42b275e2d57cdc972c08f49b7393f7b
RomanSokhan <romans@softwareplanet.uk.com>
2015-06-24 15:06:25 +0300

    Timeline hint on Entries page in winner_selection status

--- app/view_objects/entries_page.rb
+++ app/view_objects/entries_page.rb
@@ -4,7 +4,7 @@ class EntriesPage < ContestPage
     super
 
     @won_contest_request = contest.response_winner
-    @show_submissions = contest.submission? && (requests_present? || comments_present?)
+    @show_submissions = (contest.submission? && (requests_present? || comments_present?)) || contest.winner_selection?
 
     if won_contest_request
       @entries_concept_board_page = EntriesConceptBoard.new({
@@ -24,6 +24,20 @@ class EntriesPage < ContestPage
   def show_answer_options?
     contest.winner_selection? || contest.submission?
   end
+
+  def timeline_hint
+    if contest.submission?
+      I18n.t('client_center.entries.submission.day_left_to_submit',
+        time_left: view_context.distance_of_time_in_words(Time.current,
+                                             contest.phase_end,
+                                             false,
+                                             highest_measure_only: true))
+    elsif contest.winner_selection?
+      I18n.t('client_center.entries.winner_selection.hint')
+    end
+  end
+
+
 
   attr_reader :won_contest_request, :entries_concept_board_page, :visible_image_items, :share_url
 
