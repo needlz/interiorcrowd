62656b082a89a0ca422524948060ab9ed5af99fb
RomanSokhan <romans@softwareplanet.uk.com>
2015-07-10 11:26:42 +0300

    Make beta area public

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,41 +8,6 @@ class HomeController < ApplicationController
     render
   end
 
-  def sign_up_beta
-    render layout: 'sign_up'
-  end
-
-  def sign_in_beta
-    @incoming_url = params[:url]
-
-    if beta_access_granted?
-      unless params[:url].nil?
-        return redirect_to params[:url]
-      else
-        return redirect_to root_path
-      end
-    end
-
-    render layout: 'sign_up'
-  end
-
-  def create_beta_session
-    if beta_authentication(params[:password].strip)
-      cookies.permanent.signed[:beta] = true
-    else
-      flash[:error] = "Wrong password"
-    end
-
-    if params[:incoming_url].nil?
-      redirect_to root_path
-    else
-      redirect_to params[:incoming_url]
-    end
-  end
-
   private
 
-  def beta_authentication(password)
-    password == ENV['BETA_PASSWORD']
-  end
 end
