7119cc8f8cbbca96f2f1c7ce5f6f1167a9291ea9
butumusho <danuljak@gmail.com>
2015-07-27 11:25:57 +0300

    Changed function name

--- app/view_objects/client_menu.rb
+++ app/view_objects/client_menu.rb
@@ -4,14 +4,14 @@ class ClientMenu < Menu
     specific_items =
       { I18n.t('header.client_center') => view_context.client_center_index_path,
         I18n.t('header.sign_out') => view_context.logout_sessions_path }
-    common_menu_for specific_items
+    common_menu_with specific_items
   end
 
   def mobile_items
     specific_items =
       { I18n.t('header.client_center') => Navigation::ClientCenter.new.to_mobile_menu,
         I18n.t('header.sign_out') => view_context.logout_sessions_path }
-    common_menu_for specific_items
+    common_menu_with specific_items
   end
 
 end
