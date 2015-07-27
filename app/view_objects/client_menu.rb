7925fc248a6e80516947f8c9849698a9b41d2f2e
butumusho <danuljak@gmail.com>
2015-07-27 10:16:33 +0300

    Refactored site menu code

--- app/view_objects/client_menu.rb
+++ app/view_objects/client_menu.rb
@@ -14,15 +14,4 @@ class ClientMenu < Menu
     common_menu(specific_items)
   end
 
-  private
-
-  def common_menu(specific_items)
-    menu = MenuBuilder.new
-    menu.append(I18n.t('header.for_designers') => view_context.coming_soon_path)
-    menu.append(I18n.t('header.get_inspired') => 'http://blog.interiorcrowd.com/category/get-ideas')
-    menu.append(I18n.t('header.blog') => ApplicationController::BLOG_URL)
-    menu.append(specific_items)
-    menu
-  end
-
 end
--- app/view_objects/menu.rb
+++ app/view_objects/menu.rb
@@ -10,6 +10,17 @@ class Menu
     menu_class.new(current_user, view_context)
   end
 
+  protected
+
+  def common_menu(specific_items)
+    menu = MenuBuilder.new
+    menu.append(I18n.t('header.for_designers') => view_context.coming_soon_path)
+    menu.append(I18n.t('header.get_inspired') => 'http://blog.interiorcrowd.com/category/get-ideas')
+    menu.append(I18n.t('header.blog') => ApplicationController::BLOG_URL)
+    menu.append(specific_items)
+    menu
+  end
+
   private
 
   attr_reader :current_user, :view_context
