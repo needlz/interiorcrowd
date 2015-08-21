afb9d17e39c7ddb9611b6d56ab57e7ff7e52971d
RomanSokhan <romans@softwareplanet.uk.com>
2015-08-21 22:03:13 +0300

    Fix time representation on the timeline hint on the designer side

--- app/view_objects/contest_request_milestones/draft.rb
+++ app/view_objects/contest_request_milestones/draft.rb
@@ -3,10 +3,6 @@ module ContestRequestMilestones
   class Draft < Base
 
     def designer_hint
-      time_left = view_context.distance_of_time_in_words(Time.current,
-                                                         contest.phase_end,
-                                                         false,
-                                                         highest_measure_only: true)
       I18n.t('designer_center.milestones.draft.time_left', time_left: time_left)
     end
 
