5ac08ef36f4833260aa923fba2269a978f788ef1
butumusho <danuljak@gmail.com>
2015-08-21 17:58:26 +0300

    Changed view object instance name.

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -84,7 +84,7 @@ class ContestsController < ApplicationController
 
   def payment_details
     return redirect_to client_login_sessions_path unless current_user.client?
-    @card_view = CreditCardView.new(nil)
+    @shared_card_view = CreditCardView.new(nil)
     @client = current_user
     ActiveRecord::Associations::Preloader.new.preload(@client, :primary_card)
     @card_views = @client.credit_cards.map{ |credit_card| CreditCardView.new(credit_card) }
