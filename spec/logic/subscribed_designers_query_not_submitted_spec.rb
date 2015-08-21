eb71dcb0f6ac2031fd0a62b2b619bf97ae9055c6
RomanSokhan <romans@softwareplanet.uk.com>
2015-08-21 20:37:15 +0300

    Notify about updates in a contest only invited and subscribed designers

--- spec/logic/subscribed_designers_query_not_submitted_spec.rb
+++ spec/logic/subscribed_designers_query_not_submitted_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe SubscribedDesignersQueryNotSubmitted do
 
   let(:client) { Fabricate(:client) }
   let(:contest) { Fabricate(:contest, client: client, status: 'submission') }
-  let(:designer_with_submitted_contest_request) { Fabricate(:designer) }
-  let(:designer_without_submitted_contest_request) { Fabricate(:designer) }
-  let(:designer_with_contest_comment) { Fabricate(:designer) }
+  let!(:designer_with_submitted_contest_request) { Fabricate(:designer) }
+  let!(:designer_without_submitted_contest_request) { Fabricate(:designer) }
+  let!(:designer_with_contest_comment) { Fabricate(:designer) }
+  let!(:invited_designer) { Fabricate(:designer) }
+  let!(:not_invited_designer) { Fabricate(:designer) }
   let!(:submitted_contest_request) { Fabricate(:contest_request,
                                               contest: contest,
                                               designer: designer_with_submitted_contest_request) }
@@ -16,12 +18,16 @@ RSpec.describe SubscribedDesignersQueryNotSubmitted do
                                           status: 'draft') }
   let!(:contest_note) { contest.notes.create!(designer_id: designer_with_contest_comment.id,
                                               text: 'a comment') }
+  let!(:designer_invitation) { Fabricate(:designer_invite_notification,
+                                         designer: invited_designer,
+                                         contest: contest) }
 
   it 'returns subscribed designers without submitted requests' do
     query = SubscribedDesignersQueryNotSubmitted.new(contest)
     expect(query.designers).to match_array(
       [designer_without_submitted_contest_request,
-       designer_with_contest_comment]
+       designer_with_contest_comment,
+       invited_designer]
     )
   end
 
