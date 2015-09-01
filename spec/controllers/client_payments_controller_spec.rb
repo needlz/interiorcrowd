ac00d2596801251e99d0d8da7200b9d9685933e2
RomanSokhan <romans@softwareplanet.uk.com>
2015-09-01 16:14:26 +0300

    Perform payment on the crdit cards page

--- spec/controllers/client_payments_controller_spec.rb
+++ spec/controllers/client_payments_controller_spec.rb
@@ -8,13 +8,17 @@ RSpec.describe ClientPaymentsController do
   let(:contest) { Fabricate(:contest, client: client) }
   let(:credit_card) { Fabricate(:credit_card, client: client) }
 
+  before do
+    mock_stripe_customer_registration
+  end
+
   describe 'POST create' do
     before do
       sign_in(client)
       client.update_attributes!(primary_card_id: credit_card.id)
     end
 
-    context 'valid contest id' do
+    context 'when client has primary card' do
       context 'successful charge' do
         before do
           mock_stripe_successful_charge
@@ -40,8 +44,17 @@ RSpec.describe ClientPaymentsController do
       end
     end
 
-    context 'invalid context id' do
+    context 'when client has no primary card' do
+      before do
+        client.update_attributes!(primary_card_id: nil)
+        mock_stripe_successful_charge
+      end
 
+      it 'does not create payment' do
+        post :create, contest_id: contest.id
+        expect(response).to redirect_to(payment_details_contests_path(id: contest.id))
+        expect(contest.client_payment).to be_nil
+      end
     end
   end
 
