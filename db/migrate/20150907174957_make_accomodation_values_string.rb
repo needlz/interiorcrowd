d49b86e7c1460d4631536c7110cc5a4fafa4eb5c
WIKI <wiki@ubuntu.ubuntu-domain>
2015-09-08 18:45:00 +0300

    Make optional preferences of a contest be blank by default

--- db/migrate/20150907174957_make_accomodation_values_string.rb
+++ db/migrate/20150907174957_make_accomodation_values_string.rb
@@ -2,12 +2,20 @@ class MakeAccomodationValuesString < ActiveRecord::Migration
   def up
     change_column :contests, :accommodate_children, :string, null: true
     change_column :contests, :accommodate_pets, :string, null: true
+    change_column :contests, :durability, :integer, null: true
+    change_column :contests, :entertaining, :integer, null: true
   end
 
   def down
     ActiveRecord::Base.connection.execute 'ALTER TABLE contests ALTER COLUMN accommodate_children DROP DEFAULT;'
     ActiveRecord::Base.connection.execute 'ALTER TABLE contests ALTER COLUMN accommodate_pets DROP DEFAULT;'
-    change_column :contests, :accommodate_children, 'boolean USING (CASE accommodate_children WHEN \'true\' THEN \'t\'::boolean ELSE \'f\'::boolean END)'
-    change_column :contests, :accommodate_pets, 'boolean USING (CASE accommodate_pets WHEN \'true\' THEN \'t\'::boolean ELSE \'f\'::boolean END)'
+    ActiveRecord::Base.connection.execute 'ALTER TABLE contests ALTER COLUMN durability DROP DEFAULT;'
+    ActiveRecord::Base.connection.execute 'ALTER TABLE contests ALTER COLUMN entertaining DROP DEFAULT;'
+    change_column :contests,
+                  :accommodate_children,
+                  :boolean
+    change_column :contests,
+                  :accommodate_pets,
+                  :boolean
   end
 end
