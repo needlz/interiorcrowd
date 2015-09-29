79394215f2180f3a211c1d7931a1b4f8a2c40f24
RomanSokhan <romans@softwareplanet.uk.com>
2015-09-29 16:56:44 +0300

    Extract breadcrumb captions to the locale file

--- app/view_objects/breadcrumbs/client.rb
+++ app/view_objects/breadcrumbs/client.rb
@@ -3,27 +3,34 @@ module Breadcrumbs
   class Client < Base
 
     def my_contests
-      self << { name: 'My contests', href: urls_helper.client_center_entries_path }
+      self << { name: caption, href: urls_helper.client_center_entries_path }
     end
 
     def contest(contest, force_initial_phase_view = false)
       url_options = { id: contest.id }
       url_options.merge!(view: ContestPhases.phase_to_index(:initial)) if force_initial_phase_view
-      self << { name: 'Entries',
+      self << { name: caption,
                 href: urls_helper.client_center_entry_path(url_options) }
     end
 
     def profile
-      self << { name: 'Profile', href: urls_helper.client_center_profile_path }
+      self << { name: caption, href: urls_helper.client_center_profile_path }
     end
 
     def brief(contest)
-      self << { name: 'Brief', href: urls_helper.brief_contest_path(contest) }
+      self << { name: caption, href: urls_helper.brief_contest_path(contest) }
     end
 
     def contest_request(contest_request)
       possessive = contest_request.designer.first_name.possessive
-      self << { name: "#{ possessive } concept board" }
+      self << { name: caption(possessive: possessive) }
+    end
+
+    private
+
+    def caption(localization_params = nil)
+      caller_method = caller_locations(1,1)[0].label
+      I18n.t("client_center.breadcrumbs.#{ caller_method }", localization_params)
     end
 
   end
