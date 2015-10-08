5ad094b31b17dc15568d4c110c1627ab99de9db3
butumusho <danuljak@gmail.com>
2015-10-08 19:20:44 +0300

    Added card info to client's table view in Active Admin.

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -8,6 +8,26 @@ ActiveAdmin.register Client do
     f.actions
   end
 
+  index do
+    column :id
+    column :first_name
+    column :last_name
+    column :email
+    column :plain_password
+    column :status
+    column :primary_card
+    column 'Credit Cards' do |client|
+      client.credit_cards.map { |credit_card|
+        link_to 'Credit card #' + credit_card.id.to_s, admin_credit_card_path(credit_card)
+      }.join("<br />").html_safe
+    end
+    column :address
+    column :phone_number
+    column :created_at
+    column :email_opt_in
+    actions
+  end
+
   controller do
 
     include ActiveAdminExtensions::UserController
