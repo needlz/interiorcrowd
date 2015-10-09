ab19c98840e2cc496abef4b7ca4643b0a5abd3d1
butumusho <danuljak@gmail.com>
2015-10-09 14:54:18 +0300

    Changed client's index page in Active Admin.

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -15,14 +15,29 @@ ActiveAdmin.register Client do
     column :email
     column :plain_password
     column :status
+    column :zip
+    column :state
+    column :city
+    column :address
+    column :phone_number
+    column 'Billing Zip' do |client|
+      client.primary_card.zip if client.primary_card.present?
+    end
+    column 'Billing State' do |client|
+      client.primary_card.state if client.primary_card.present?
+    end
+    column 'Billing City' do |client|
+      client.primary_card.city if client.primary_card.present?
+    end
+    column 'Billing Address' do |client|
+      client.primary_card.address if client.primary_card.present?
+    end
     column :primary_card
     column 'Credit Cards' do |client|
       client.credit_cards.map { |credit_card|
         link_to 'Credit card #' + credit_card.id.to_s, admin_credit_card_path(credit_card)
       }.join("<br />").html_safe
     end
-    column :address
-    column :phone_number
     column :created_at
     column :email_opt_in
     actions
