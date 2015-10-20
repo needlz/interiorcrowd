cb9dfdda6dc3680e7e699c2f3a21c7a97a834a90
butumusho <danuljak@gmail.com>
2015-10-20 11:14:18 +0300

    Refactored code. Placed logic to the module.

--- app/admin/detailed_contest.rb
+++ app/admin/detailed_contest.rb
@@ -1,3 +1,5 @@
+include ActiveAdminExtensions::ContestDetails
+
 ActiveAdmin.register Contest, as: "Detailed Contest" do
   actions :all, :except => [:new, :destroy, :edit, :show]
 
@@ -7,34 +9,19 @@ ActiveAdmin.register Contest, as: "Detailed Contest" do
       link_to contest.id, admin_contest_path(contest)
     end
     column 'Contest' do |contest|
-      client = contest.client
-      full_contest_name = link_to(client.first_name.to_s + ' ' + client.last_name.to_s.possessive,
-                                  admin_client_path(client)) + ' ' + contest.project_name + " (##{contest.id})"
-      full_contest_name.html_safe
+      contest_name(contest)
     end
     column 'Start Date' do |contest|
       contest.submission_started_at
     end
     column 'Designers' do |contest|
-      contest.requests.ever_published.map do |request|
-        designer = request.designer
-        statement = link_to(designer.first_name.to_s + ' ' + designer.last_name.to_s, admin_designer_path(designer))
-        statement = statement + ', submitted at ' + request.submitted_at.to_s if request.submitted_at
-        statement
-      end.join('<br />').html_safe
+      designers_list(contest)
     end
     column 'Winner' do |contest|
-      if contest.response_winner
-        designer = contest.response_winner.designer
-        statement = link_to(designer.first_name.to_s + ' ' + designer.last_name.to_s, admin_designer_path(designer))
-        statement = statement + ', won at ' + contest.response_winner.won_at.to_s if contest.response_winner.won_at
-        statement.html_safe
-      end
+      winner_info(contest)
     end
     column 'End Date' do |contest|
-      if %w[finished closed].include? contest.status
-        contest.status.to_s.capitalize + ' at ' + contest.finished_at.to_s if contest.finished_at
-      end
+      end_date(contest)
     end
   end
 
---
+++ lib/active_admin_extensions/contest_details.rb
@@ -0,0 +1,49 @@
+module ActiveAdminExtensions
+
+  module ContestDetails
+    def end_date(contest)
+      if %w[finished closed].include? contest.status
+        formatted_date(contest.status.to_s.capitalize + ' at ', contest.finished_at)
+      end
+    end
+
+    def contest_name(contest)
+      client = contest.client
+      full_contest_name = link_to(full_user_name(client).possessive, admin_client_path(client)) + append_project_name(contest)
+      full_contest_name.html_safe
+    end
+
+    def full_user_name(user)
+      return user.first_name.to_s unless user.last_name
+      return user.last_name.to_s unless user.first_name
+      user.first_name + ' ' + user.last_name
+    end
+
+    def append_project_name(contest)
+      ' ' + contest.project_name.to_s + " (##{contest.id})"
+    end
+
+    def designers_list(contest)
+      contest.requests.ever_published.map do |request|
+        designer = request.designer
+        statement = link_to(full_user_name(designer), admin_designer_path(designer))
+        statement = statement + formatted_date(', submitted at ', request.submitted_at)
+        statement
+      end.join('<br />').html_safe
+    end
+
+    def formatted_date(status_prefix, date)
+      status_prefix + date.to_s if date
+    end
+
+    def winner_info(contest)
+      if contest.response_winner
+        designer = contest.response_winner.designer
+        statement = link_to(full_user_name(designer), admin_designer_path(designer))
+        statement = statement + formatted_date(', won at ', contest.response_winner.won_at)
+        statement.html_safe
+      end
+    end
+  end
+
+end
