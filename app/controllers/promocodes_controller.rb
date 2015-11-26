2a64ac878cfe600ddb1f21a7a01195a15835c806
RomanSokhan <romans@softwareplanet.uk.com>
2015-11-26 12:02:10 +0200

    Show custom message on promocode applied

--- app/controllers/promocodes_controller.rb
+++ app/controllers/promocodes_controller.rb
@@ -9,7 +9,10 @@ class PromocodesController < ApplicationController
   def check_promocode(promocode)
     code = Promocode.active.find_by_promocode(promocode)
     result = { valid: code.present? }
-    result.merge!(profit: code.display_message, discount: code.discount_cents / 100) if code
+    if code
+      display_message = code.display_message % { discount_dollars: view_context.humanized_money_with_symbol(code.discount) }
+      result.merge!(display_message: display_message, discount: code.discount_cents / 100)
+    end
     result
   end
 
