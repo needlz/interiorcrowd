c5c4b5a13f3d397f54a6fd2b1a2597324483d2fa
butumusho <danuljak@gmail.com>
2015-11-26 12:28:50 +0200

    CopyDesignersContestNotesToContestRequestsComments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118181131) do
+ActiveRecord::Schema.define(version: 20151126084800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
