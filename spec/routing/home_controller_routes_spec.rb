b30588f715482e0c7dccf99f702c3a1a8c9a07ad
butumusho <danuljak@gmail.com>
2015-12-15 19:51:15 +0200

    Added 'About Us' page.

--- spec/routing/home_controller_routes_spec.rb
+++ spec/routing/home_controller_routes_spec.rb
@@ -1,8 +1,13 @@
 require "rails_helper"
 
 RSpec.describe HomeController do
-  it 'routes GET request to "How It Works" page' do
+  it 'routes GET #how_it_works request to "How It Works" page' do
     expect(get: 'how_it_works').to route_to(action: 'how_it_works',
                                             controller: 'home')
+  end
+
+  it 'routes GET #about_us request to "About Us" page' do
+    expect(get: 'about_us').to route_to(action: 'about_us',
+                                        controller: 'home')
   end
 end
