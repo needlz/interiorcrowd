a172412a8fd2314f39d49fd90dac4a94addc50fc
RomanSokhan <romans@softwareplanet.uk.com>
2016-01-11 21:10:56 +0200

    Do not show contest a designer already submitted to in list of recommended cotnests

--- app/models/available_contests_query.rb
+++ app/models/available_contests_query.rb
@@ -12,6 +12,19 @@ class AvailableContestsQuery
     designer.invited_contests.current.all
   end
 
+  def suggested
+    contests = Contest.arel_table
+    contest_requests = ContestRequest.arel_table
+
+    requests_submitted_by_designer = contest_requests.project(Arel.sql('id')).
+        where(contest_requests[:contest_id].eq(contests[:id]).
+            and(contest_requests[:status].not_eq('draft').
+            and(contest_requests[:designer_id].eq(designer.id)))
+        )
+
+    Contest.current.joins('LEFT JOIN "contest_requests" ON "contest_requests"."contest_id" = "contests"."id"').where.not(requests_submitted_by_designer.exists)
+  end
+
   private
 
   attr_reader :designer
--- spec/models/available_contests_query_spec.rb
+++ spec/models/available_contests_query_spec.rb
@@ -7,17 +7,40 @@ RSpec.describe AvailableContestsQuery do
 
   let(:closed_contest) { Fabricate(:contest, client: client, status: 'closed') }
   let(:submission_contest) { Fabricate(:contest, client: client, status: 'submission') }
+  let(:submission_contest_with_draft_request) do
+    contest = Fabricate(:contest, client: client, status: 'submission')
+    Fabricate(:contest_request, contest: contest, designer: designer, status: 'draft')
+    contest
+  end
+  let(:submission_contest_with_submitted_request) do
+    contest = Fabricate(:contest, client: client, status: 'submission')
+    Fabricate(:contest_request, contest: contest, designer: designer, status: 'submitted')
+    contest
+  end
   let(:invited_contest) { Fabricate(:contest, client: client, status: 'submission') }
   let(:winner_selection_contest) { Fabricate(:contest, client: client, status: 'winner_selection') }
   let(:fulfillment_contest) { Fabricate(:contest, client: client, status: 'fulfillment') }
   let(:finished_contest) { Fabricate(:contest, client: client, status: 'finished') }
 
-  let(:contests) { [closed_contest, winner_selection_contest, fulfillment_contest, finished_contest, submission_contest] }
+  let(:contests) { [closed_contest,
+                    winner_selection_contest,
+                    fulfillment_contest,
+                    finished_contest,
+                    submission_contest,
+                    submission_contest_with_draft_request,
+                    submission_contest_with_submitted_request ] }
 
   let(:available_contests) { AvailableContestsQuery.new(designer) }
 
+  before do
+    contests
+  end
+
   it 'returns list of active contests' do
-    expect(available_contests.all).to match_array [submission_contest, invited_contest]
+    expect(available_contests.all).to match_array [submission_contest,
+                                                   invited_contest,
+                                                   submission_contest_with_draft_request,
+                                                   submission_contest_with_submitted_request]
   end
 
   context 'invited to contests' do
@@ -30,6 +53,10 @@ RSpec.describe AvailableContestsQuery do
     it 'returns list of current contests a designer was invited to' do
       expect(available_contests.invited).to match_array [invited_contest]
     end
+  end
+
+  it 'returns list of suggested contests' do
+    expect(available_contests.suggested).to match_array [submission_contest, submission_contest_with_draft_request]
   end
 
 end
