1bb2cbb3bd2dd14fefea40f61b93a9c58bcfdd83
RomanSokhan <romans@softwareplanetgroup.com>
2016-03-24 17:10:45 +0200

    Use proxy to fetch blog pages only if needed.

--- lib/blog/redirect_follower.rb
+++ lib/blog/redirect_follower.rb
@@ -2,42 +2,79 @@ module Blog
 
   class RedirectFollower
 
+    BLOG_HOSTING_ACCESS_DENIED_MSG = 'If you feel this is in error please contact your hosting providers abuse department'
+
     def initialize(options)
       @faraday_response = options[:faraday_response]
       @default_referer = options[:default_referer]
       @blog_path = options[:blog_path]
       @original_url = options[:original_url]
       @params = options[:params]
+      @last_url_in_redirects_chain = @original_url
     end
 
     def final_response(&on_request)
-      if responded_with_redirect?(faraday_response)
-        redirect = faraday_response.headers[:location]
-        redirect = redirect.gsub(/(\?|&)?icrowd_app=yes/, '')
-        if relative_path?(redirect)
-          referer = get_referer_path(redirect)
-          redirect = referer + '/' + redirect
-        end
-        if equal_to_request_url_with_slash?(redirect)
-          @faraday_response = on_request.call(redirect)
-          return final_response(&on_request)
-        end
-        redirect_matches = redirect.match(/(#{ Regexp.escape(URI(default_referer).host) })\/(.+)/)
-        redirect_part = redirect_matches[2] if redirect_matches
-        redirect_part[0] = '' if redirect_part[0] == '/' if redirect_part
-        redirect = blog_path + redirect_part.to_s
-        redirect
-      else
-        faraday_response
-      end
+      @request_performer = on_request
+      do_request
     end
 
     private
 
-    attr_reader :faraday_response, :default_referer, :blog_path, :original_url, :params
+    attr_reader :faraday_response, :default_referer, :blog_path, :original_url, :params, :session
+
+    attr_accessor :redirect
+
+    def handle_response
+      if responded_with_redirect?(faraday_response)
+        handle_redirect
+      else
+        if turn_proxy_on?
+          session[:use_blog_proxy] = true
+          do_request
+        else
+          faraday_response
+        end
+      end
+    end
+
+    def turn_proxy_on?
+      faraday_response.headers['content-type'] == 'text/html' &&
+        faraday_response.body.include?(BLOG_HOSTING_ACCESS_DENIED_MSG) &&
+        !session[:use_blog_proxy]
+    end
+
+    def handle_redirect
+      self.redirect = faraday_response.headers[:location]
+      self.redirect = redirect.gsub(/(\?|&)?icrowd_app=yes/, '')
+      complement_redirect_location
+      if equal_to_request_url_with_slash?(redirect)
+        @last_url_in_redirects_chain = redirect
+        return do_request
+      end
+      replace_host
+      redirect
+    end
+
+    def complement_redirect_location
+      if relative_path?(redirect)
+        referer = get_referer_path(redirect)
+        self.redirect = referer + '/' + redirect
+      end
+    end
+
+    def do_request
+      @faraday_response = @request_performer.call(@last_url_in_redirects_chain)
+      handle_response
+    end
 
     def responded_with_redirect?(faraday_response)
       faraday_response.status / 100 == 3
+    end
+
+    def replace_host
+      redirect_matches = redirect.match(/(#{ Regexp.escape(URI(default_referer).host) })\/(.+)/)
+      redirect_part = redirect_matches[2] if redirect_matches
+      self.redirect = blog_path + redirect_part.to_s
     end
 
     def equal_to_request_url_with_slash?(url)
