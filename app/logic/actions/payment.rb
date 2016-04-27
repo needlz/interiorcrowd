4f51e506042a2b88061b3a624cdfe881fb63e1bf
RomanSokhan <romans@softwareplanetgroup.com>
2016-04-27 18:28:41 +0300

    Do not perform Stripe charge if final price is zero

--- app/logic/actions/payment.rb
+++ app/logic/actions/payment.rb
@@ -1,66 +1,74 @@
 class Payment
 
   DEFAULT_CURRENCY = 'USD'
+  ZERO_PRICE_PLACEHOLDER = 'zero price'
 
-  attr_reader :client_payment, :error_message, :exception
+  attr_reader :client_payment
 
   def initialize(contest)
     @contest = contest
     @client = contest.client
-    @error = nil
   end
 
   def perform
     StripeCustomer.fill_client_info(client)
-    customer = StripeCustomer.new(client)
-    unless charge_already_performed?
-      calculator = PriceCalculator.new(contest: contest)
-      price = calculator.price_in_cents
-      @client_payment = contest.client_payment
-      card = client.primary_card
-      raise ArgumentError.new('Primary card not set') unless card
+    return if charge_already_performed?
 
-      if client_payment
-        client_payment.update_attributes!(payment_status: 'pending',
-                                   client_id: client.id,
-                                   contest_id: contest.id,
-                                   amount_cents: price,
-                                   promotion_cents: calculator.promotion_in_cents,
-                                   credit_card_id: card.id)
-      else
-        @client_payment = ClientPayment.create!(payment_status: 'pending',
-                                        client_id: client.id,
-                                        contest_id: contest.id,
-                                        amount_cents: price,
-                                        promotion_cents: calculator.promotion_in_cents,
-                                        credit_card_id: card.id)
-      end
-      begin
-        amount = Money.new(price, DEFAULT_CURRENCY)
-        description = "charge client with id #{ client.id }"
-        @charge = customer.charge(money: amount,
-                                 description: description,
-                                 card_id: card.stripe_id)
-        update_payment
-        contest.reload
-        submit_contest
-      rescue StandardError => e
-        @error_message = e.message + "\n" + e.backtrace.join("\n")
-        client_payment.update_attributes!(last_error: @error_message)
-        raise
-      end
+    @credit_card = client.primary_card
+    raise ArgumentError.new('Primary card not set') unless credit_card
+
+    @calculator = PriceCalculator.new(contest: contest)
+    @price = calculator.price_in_cents
+    @client_payment = contest.client_payment
+
+    initialize_payment_record
+    begin
+      perform_stripe_charge
+      finalize_payment_record
+      contest.reload
+      submit_contest
+    rescue StandardError => e
+      client_payment.update_attributes!(last_error: e.message + "\n" + e.backtrace.join("\n"))
+      raise
+    end
+  end
+
+  def initialize_payment_record
+    attributes = { payment_status: 'pending',
+                   client_id: client.id,
+                   contest_id: contest.id,
+                   amount_cents: price,
+                   promotion_cents: calculator.promotion_in_cents,
+                   credit_card_id: credit_card.id }
+    if client_payment
+      client_payment.update_attributes!(attributes)
+    else
+      @client_payment = ClientPayment.create!(attributes)
     end
   end
 
   private
 
-  attr_reader :contest, :client, :charge
+  attr_reader :contest, :client, :charge, :price, :calculator, :credit_card
 
   def charge_already_performed?
     contest.paid?
   end
 
-  def update_payment
+  def perform_stripe_charge
+    if price <= 0
+      @charge = Hashie::Mash.new({ id: ZERO_PRICE_PLACEHOLDER })
+    else
+      customer = StripeCustomer.new(client)
+      amount = Money.new(price, DEFAULT_CURRENCY)
+      description = "charge client with id #{ client.id }"
+      @charge = customer.charge(money: amount,
+                                description: description,
+                                card_id: credit_card.stripe_id)
+    end
+  end
+
+  def finalize_payment_record
     client_payment.update_attributes!(payment_status: 'completed',
                                       stripe_charge_id: charge.id,
                                       last_error: nil)
--- spec/logic/payment_spec.rb
+++ spec/logic/payment_spec.rb
@@ -11,14 +11,32 @@ RSpec.describe Payment do
     mock_stripe_customer_registration
   end
 
-  context 'when successful charge' do
-    before do
+  context 'when package is free' do
+    let(:promocode) { Fabricate(:promocode, discount_cents: BudgetPlan::PLANS[0].price_in_cents) }
+
+    it 'creates client_payment' do
+      expect_any_instance_of(StripeCustomer).to_not receive(:charge)
       mock_stripe_successful_charge
       Payment.new(contest).perform
       contest.reload
-    end
 
+      expect(contest.client_payment.client_id).to eq(client.id)
+      expect(contest.client_payment.amount_cents).to eq(contest.package.price_in_cents - promocode.discount_cents)
+      expect(contest.client_payment.promotion_cents).to eq(promocode.discount_cents)
+      expect(contest.client_payment.last_error).to be_blank
+      expect(contest.client_payment.payment_status).to eq('completed')
+      expect(contest.client_payment.credit_card_id).to eq(credit_card.id)
+      expect(contest.client_payment.stripe_charge_id).to eq(Payment::ZERO_PRICE_PLACEHOLDER)
+    end
+  end
+
+  context 'when package is not free' do
     it 'creates client_payment' do
+      expect_any_instance_of(StripeCustomer).to receive(:charge)
+      mock_stripe_successful_charge
+      Payment.new(contest).perform
+      contest.reload
+
       expect(contest.client_payment.client_id).to eq(client.id)
       expect(contest.client_payment.amount_cents).to eq(contest.package.price_in_cents - promocode.discount_cents)
       expect(contest.client_payment.promotion_cents).to eq(promocode.discount_cents)
