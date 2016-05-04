979dc7918c482fb3ed5413ef64a0f6ee45db9e9a
RomanSokhan <romans@softwareplanetgroup.com>
2016-05-04 17:55:50 +0300

    Validations for the date range of the designer activity

--- app/models/designer_activity.rb
+++ app/models/designer_activity.rb
@@ -4,5 +4,14 @@ class DesignerActivity < ActiveRecord::Base
   has_many :comments, class_name: 'DesignerActivityComment'
 
   validates_presence_of :hours, :start_date, :due_date
+  validate :due_date_after_start_date
+
+  private
+
+  def due_date_after_start_date
+    if due_date < start_date
+      errors.add(:due_date, 'must be after start_date')
+    end
+  end
 
 end
--- spec/models/designer_activity_spec.rb
+++ spec/models/designer_activity_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe DesignerActivity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:designer_activity) { Fabricate(:designer_activity) }
+
+  describe 'updating attributes' do
+    context 'when due_date before start_date' do
+      it 'creates error message' do
+        designer_activity.update_attributes(start_date: DateTime.now, due_date: DateTime.now - 3.days)
+        expect(designer_activity.errors.messages[:due_date]).to be_present
+      end
+    end
+
+    context 'when due_date after start_date' do
+      it 'does not create error message' do
+        designer_activity.update_attributes(start_date: DateTime.now, due_date: DateTime.now + 3.days)
+        expect(designer_activity.errors.messages[:due_date]).to_not be_present
+      end
+    end
+  end
+
+
+
 end
