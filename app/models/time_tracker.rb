99830359a8784460dd4c30c1ba82a0e743f399ba
Yurii Danyliak <yuriid@softwareplanetgroup.com>
2016-05-04 17:42:37 +0300

    Implement Stripe checkout.

--- app/models/time_tracker.rb
+++ app/models/time_tracker.rb
@@ -1,6 +1,7 @@
 class TimeTracker < ActiveRecord::Base
   belongs_to :contest
   has_many :designer_activities
+  has_many :hourly_payments
   has_and_belongs_to_many :attachments,
                           class_name: 'Image',
                           join_table: 'time_trackers_attachments',
@@ -8,9 +9,7 @@ class TimeTracker < ActiveRecord::Base
 
   validates :hours_suggested, :numericality => { :greater_than_or_equal_to => 0 }
 
-  PRICE_PER_HOUR = 100
-
   def price_per_hour
-    PRICE_PER_HOUR
+    Settings.hour_with_designer_price.to_i
   end
 end
