7ac5111394e6d1f8c885fb9807495688e626f763
RomanSokhan <romans@softwareplanetgroup.com>
2016-05-06 21:29:29 +0300

    Ability to add multiple designer activities at once

--- app/forms/designer_activity_form.rb
+++ app/forms/designer_activity_form.rb
@@ -10,7 +10,7 @@ class DesignerActivityForm
     delegate attr.to_sym, "#{ attr }=".to_sym, to: :activity
   end
 
-  attr_reader :activity
+  attr_reader :activity, :params
 
   def initialize(activity_or_params = nil)
     if activity_or_params
@@ -18,24 +18,30 @@ class DesignerActivityForm
         @activity = activity_or_params
       else
         @params = ActionController::Parameters.new(activity_or_params)
-        @params[:designer_activity][:start_date] = Date.strptime(@params[:designer_activity][:start_date], "%m/%d/%Y")
-        @params[:designer_activity][:due_date] = Date.strptime(@params[:designer_activity][:due_date], "%m/%d/%Y")
+        @params[:designer_activity][:start_date] = Date.strptime(params[:designer_activity][:start_date], "%m/%d/%Y")
+        @params[:designer_activity][:due_date] = Date.strptime(params[:designer_activity][:due_date], "%m/%d/%Y")
       end
     else
       @activity = DesignerActivity.new(hours: 1)
     end
   end
 
-  def activity_comment_attributes
-    @params[:designer_activity].try(:[], :comments)
+  def tasks
+    [@activity]
+  end
+
+  def tasks_attributes=(attributes)
   end
 
   def self.model_name
     DesignerActivity.model_name
   end
 
-  def activity_attributes
-    @params.require(:designer_activity).permit(*self.class.activity_attributes)
+  def activities_params
+    params[:designer_activity][:tasks_attributes].map { |id, task_params|
+      task_params.merge(start_date: params[:designer_activity][:start_date],
+                        due_date: params[:designer_activity][:start_date])
+    }
   end
 
 end
