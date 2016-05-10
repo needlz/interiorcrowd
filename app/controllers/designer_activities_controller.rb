df2b05d5edd917a081d1027683d3e253460c74c2
RomanSokhan <romans@softwareplanetgroup.com>
2016-05-10 17:54:25 +0300

    UI validations for new activity form in time tracker

--- app/controllers/designer_activities_controller.rb
+++ app/controllers/designer_activities_controller.rb
@@ -6,17 +6,20 @@ class DesignerActivitiesController < ApplicationController
 
       activity_form = DesignerActivityForm.new(params)
 
-      activities = activity_form.activities_params.map { |activity_params|
-        activity = tracker.designer_activities.create!(designer_activity_params(activity_params))
-        comment_attributes = activity_params.try(:[], :comments)
-        if comment_attributes.try(:[], :text).present?
-          activity.comments.create(comment_attributes.merge(author: current_user))
+      activities_creation_results = activity_form.activities_params.map { |activity_params|
+        activity = DesignerActivity.new(designer_activity_params(activity_params))
+        if activity.save
+          comment_attributes = activity_params.try(:[], :comments)
+          if activity.persisted? && comment_attributes.try(:[], :text).present?
+            activity.comments.create(comment_attributes.merge(author: current_user))
+          end
+          tracker.designer_activities << activity
         end
-        activity
+        {
+          temporary_id: activity_params[:temporary_id],
+          activity: activity
+        }
       }
-
-      week = activities.first.start_date.at_end_of_week
-      week_id = week.at_beginning_of_week.to_i
     rescue StandardError => e
       log_error(e)
       render status: :server_error, json: t('time_tracker.designer.request_send_error')
@@ -27,18 +30,29 @@ class DesignerActivitiesController < ApplicationController
         { group_id: group.group_id,
           title_html: render_to_string(partial: 'time_tracker/activities_group_title', locals: { group_view: group }) }
       }
-      responses = activities.map { |activity|
-        group_view = groups_holder.group_by_activity(activity)
-        { new_activity_html: render_to_string(partial: 'time_tracker/activity',
-                                              locals: { activity_view: DesignerActivityView.new(activity, current_user),
-                                                        collapsed: false }),
-          id: activity.id,
-          group_id: week_id,
-          group_header_html: render_to_string(partial: 'time_tracker/group_header',
-                                              locals: { group_view: group_view,
-                                                        collapsed: false,
-                                                        header_only: true }) }
-      }
+
+      responses = activities_creation_results.map do |activity_creation_result|
+        activity = activity_creation_result[:activity]
+        if activity.persisted?
+          group_view = groups_holder.group_by_activity(activity)
+          { new_activity_html: render_to_string(partial: 'time_tracker/activity',
+                                                locals: { activity_view: DesignerActivityView.new(activity, current_user),
+                                                          collapsed: false }),
+            id: activity.id,
+            group_id: group_view.group_id,
+            group_header_html: render_to_string(partial: 'time_tracker/group_header',
+                                                locals: { group_view: group_view,
+                                                          collapsed: false,
+                                                          header_only: true }),
+            temporary_id: activity_creation_result[:temporary_id]
+          }
+        else
+          {
+            error: activity.errors.messages,
+            temporary_id: activity_creation_result[:temporary_id]
+          }
+        end
+      end
       render status: :ok, json: { activities: responses, groups_titles_html: groups_titles_html }
     end
   end
