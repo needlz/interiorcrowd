e0124e5a8d01fa0326f038592dc58914c88b3ff0
Yurii Danyliak <yuriid@softwareplanetgroup.com>
2016-05-11 16:29:34 +0300

    Remove render-blocking CSS.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,7 @@ class HomeController < ApplicationController
     session[:return_to] = nil
     session[:login_after] = nil
     @main_navigation_items_prepend = [MenuBuilder::MenuItem.new(I18n.t('header.commercial'), Settings.commercial_link)]
+    @dont_include_application_css = true
     render
   end
 
