1c9b1f0cc8aed758466cdd47020f6dc7eee3e1cd
RomanSokhan <romans@softwareplanetgroup.com>
2016-05-11 14:53:19 +0300

    Code clean up

---
+++ app/logic/actions/create_designer_activities.rb
@@ -0,0 +1,32 @@
+class CreateDesignerActivities < Action
+
+  attr_reader :activity_form, :time_tracker, :result, :user
+
+  def initialize(options)
+    @activity_form = options[:activity_form]
+    @time_tracker = options[:time_tracker]
+    @user = options[:user]
+  end
+
+  def perform
+    @result = activity_form.activities_params.map do |activity_params|
+      activity = DesignerActivity.new(designer_activity_params(activity_params))
+      if activity.save
+        comment_attributes = activity_params.try(:[], :comments)
+        if activity.persisted? && comment_attributes.try(:[], :text).present?
+          activity.comments.create(comment_attributes.merge(author: user))
+        end
+        time_tracker.designer_activities << activity
+      end
+      {
+        temporary_id: activity_params[:temporary_id],
+        activity: activity
+      }
+    end
+  end
+
+  def designer_activity_params(activity_params)
+    activity_params.permit(:start_date, :due_date, :task, :hours)
+  end
+
+end
