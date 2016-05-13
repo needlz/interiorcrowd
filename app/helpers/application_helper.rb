764388b54cc789df0a1463ee0a31d3fabfd94fce
RomanSokhan <romans@softwareplanetgroup.com>
2016-05-13 11:42:04 +0300

    Optimize rendering of translations to a js object

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
 
   def translations_to_json(sections)
     @translator = I18n.backend
-    @translator.load_translations
     if sections.present?
       if sections.kind_of?(Array)
         @translations = @translator.send(:translations)[I18n.locale]
