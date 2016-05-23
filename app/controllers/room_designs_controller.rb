d050160f7be78e213f920cd8f516153226c9956c
Yuri Danyliak <yuriid@softwareplanetgroup.com>
2016-05-23 11:33:32 +0300

    Add arrow to no room description container.

--- app/controllers/room_designs_controller.rb
+++ app/controllers/room_designs_controller.rb
@@ -2,9 +2,12 @@ class RoomDesignsController < ApplicationController
   def create
     @room_design = RoomDesign.new(room_params)
     @room_design.contest_request = ContestRequest.find(params[:contest_request_id])
-    @room_design.save!
-    render json: { new_room_html: render_to_string(partial: 'designer_center_requests/edit/room',
+    if @room_design.save!
+      render json: { new_room_html: render_to_string(partial: 'designer_center_requests/edit/room',
                                                    locals: { room: @room_design }) }
+    else
+      render status: 500, json: {}
+    end
   end
 
   private
